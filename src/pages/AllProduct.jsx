d1b2d0ef889d3a6f09066c79a270254ac6319ab1
mitesh <miteshduiux@gmail.com>
2023-03-07 11:36:56 +0530

    Status Column Add

--- src/pages/AllProduct.jsx
+++ src/pages/AllProduct.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-lone-blocks */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from "react";
@@ -6,7 +7,87 @@ import { toast } from "react-toastify";
 import { fetchProduct } from "../features/product/productSlice";
 
 //MaterialUI Components import...
-import { Paper, Table, TableContainer, TableHead, TableRow } from "@mui/material";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+
+const columns = [
+  {
+    id: "prodImage",
+    label: "Image",
+    width: "120px",
+    imageHeight: "200px",
+    align: "center",
+  },
+  {
+    id: "prodStatus",
+    label: "Status",
+    width: "60px",
+    align: "center",
+  },
+  {
+    id: "prodName",
+    label: "Name",
+    width: "220px",
+    align: "center",
+  },
+  {
+    id: "prodDesc",
+    label: "Description",
+    width: "220px",
+    align: "center",
+  },
+  {
+    id: "prodCategory",
+    label: "Category",
+    width: "120px",
+    align: "center",
+  },
+  {
+    id: "prodQuantity",
+    label: "Quantity",
+    width: "120px",
+    align: "right",
+  },
+  {
+    id: "prodPrice",
+    label: "Price(₹)",
+    width: "120px",
+    align: "right",
+  },
+  {
+    id: "discount",
+    label: "Discount(%)",
+    width: "120px",
+    align: "center",
+  },
+  {
+    id: "rating",
+    label: "Rating",
+    width: "120px",
+    align: "center",
+  },
+  {
+    id: "deliveryType",
+    label: "Delivery Type",
+    width: "120px",
+    align: "center",
+  },
+  // {
+  //   id: 'prodImage',
+  //   label: 'Image',
+  //   minWidth: 170,
+  //   align: 'right',
+  // },
+];
 
 function AllProduct() {
   const dispatch = useDispatch();
@@ -14,6 +95,19 @@ function AllProduct() {
   const { products, isLoading, isFetched, isError, message } = useSelector(
     (state) => state.product
   );
+
+  //For Pagination...
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
   console.log("Products : ", products);
   useEffect(() => {
@@ -32,31 +126,95 @@ function AllProduct() {
 
   return (
     <>
-      <section className="content" style={{ marginTop: "125px" }}>
+      <section className="content" style={{ marginTop: "140px" }}>
         {products.length > 0 ? (
           <div className="product">
-            {products.map((product) => (
-              <Paper
-                sx={{
-                  width: "100%",
-                  overflow: "hidden",
-                  border: "0.1px solid black",
-                }}
-              >
-                <TableContainer sx={{ maxHeight: 440 }}>
-                  <Table stickyHeader aria-label="sticky table">
-                    <TableHead>
-                      <TableRow>
-                        
-                      </TableRow>
-                    </TableHead>
-                  </Table>
-                </TableContainer>
-              </Paper>
-            ))}
+            <Paper
+              sx={{
+                width: "95%",
+                overflow: "hidden",
+                marginLeft: "2.3%",
+                border: "0.1px solid black",
+              }}
+            >
+              <TableContainer sx={{ height: "750px" }}>
+                <Table stickyHeader aria-label="sticky table">
+                  <TableHead>
+                    <TableRow>
+                      {columns.map((column) => (
+                        <TableCell
+                          key={column.id}
+                          align={column.align}
+                          style={{
+                            width: column.width,
+                            fontWeight: "bold",
+                            borderBottom: "1px solid black",
+                          }}
+                        >
+                          {column.label}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {products
+                      .slice(
+                        page * rowsPerPage,
+                        page * rowsPerPage + rowsPerPage
+                      )
+                      .map((row) => {
+                        return (
+                          <TableRow
+                            hover
+                            role="checkbox"
+                            tabIndex={-1}
+                            key={row.code}
+                            sx={{ margin: "2px", padding: "0px" }}
+                            style={{ height: "150px" }}
+                          >
+                            {  columns.map((column) => {
+                              
+                              
+                              const value = row[column.id];
+                              const status = column.prodQuantity;
+                              return (
+                                <TableCell key={column.id} align={column.align}>
+                                  {column.id === "prodStatus" ? "a" : value}
+                                </TableCell>
+                              );
+                            })}
+
+                            {/* <button>Edit</button> */}
+                          </TableRow>
+                        );
+                      })}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <TablePagination
+                rowsPerPageOptions={[10, 25, 100]}
+                component="div"
+                count={products.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+              />
+            </Paper>
           </div>
         ) : (
-          <h3>Add Products</h3>
+          <div
+            style={{
+              textAlign: "center",
+              fontFamily: "sans-serif",
+              fontSize: "35px",
+              fontWeight: "bold",
+            }}
+          >
+            {" "}
+            No Products Found, 
+            <Link to="/admin/addproduct">Click Here To Add Products</Link>
+          </div>
         )}
       </section>
     </>
